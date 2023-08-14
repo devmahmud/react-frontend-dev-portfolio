05a5f7a7a76197c1bc9e06df91481e002f367408
Mahmudul Alam <expelmahmud@gmail.com>
2023-08-14 16:23:18 +0600

    fix: vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-frontend-dev-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
